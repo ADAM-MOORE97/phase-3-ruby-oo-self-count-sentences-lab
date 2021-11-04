24ac1d29babc921a03b0846f114825f6c198b04b
Adam <adammoore118@outlook.com>
2021-11-04 11:32:27 -0400

    commit tree 1fafa55c27b7831e370d66b59dd3b4bd76730b3f

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    # self.split(/[.?!]/).filter{|sentence| !sentence.empty?}.count
+    binding.pry
+    self.split(/[.?!]/).reject{|s| s.empty?}.count
   end
 end
+
+declaration = String.new("This, well, is a sentence. This is too!! And so is this, I think? Woo...")
+
+puts declaration.count_sentences
